e45f64508fdb9c1c2f4e26080973c1623023a0d1
alsarna <48124496+alsarna@users.noreply.github.com>
2019-12-02 15:29:45 +0100

    adding and improving tests

--- tests/testthat/test-random_declustering.R
+++ tests/testthat/test-random_declustering.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+points1 = sf::st_read(system.file("points/punkty1.shp", package = "declusteringr"))
+points2 = sf::st_read(system.file("points/punkty2.shp", package = "declusteringr"))
+points3 = sf::st_read(system.file("points/punkty3.shp", package = "declusteringr"))
+points4 = sf::st_read(system.file("points/punkty4.shp", package = "declusteringr"))
+points5 = sf::st_read(system.file("points/punkty5.shp", package = "declusteringr"))
+points6 = sf::st_read(system.file("points/punkty6.shp", package = "declusteringr"))
+
+testthat::test_that("checking type",{
+  testthat::expect_type(points1, "list")
+  testthat::expect_type(points2, "list")
+  testthat::expect_type(points3, "list")
+  testthat::expect_type(points4, "list")
+  testthat::expect_type(points5, "list")
+  testthat::expect_type(points6, "list")
 })
